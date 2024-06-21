5313ac37dd6296ca5a79731c957564bc330bca53
Sobuz391 <monowerhossaisobuz@gmail.com>
2024-06-22 01:28:56 +0600

    added verifAdmin route

--- index.js
+++ index.js
@@ -53,6 +53,21 @@ async function run() {
       });
       res.send({ token });
     });
+     // Middlewares
+     const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
 
   
   } catch (err) {
