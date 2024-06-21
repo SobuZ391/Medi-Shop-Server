93dcef64cb0f925aeb70afb4386169bbf1388791
Sobuz391 <monowerhossaisobuz@gmail.com>
2024-06-22 01:34:28 +0600

    added user demotion method in server

--- index.js
+++ index.js
@@ -143,6 +143,14 @@ async function run() {
         res.send(result);
       }
     );
+    app.patch("/users/user/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = { $set: { role: "user" } };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
 
 
   
